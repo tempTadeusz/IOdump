d38ba4155de027c97175da5fe6e0b7c6ca52a388
temp <temp>
2014-05-15 17:21:35 +0200

    naprawione

--- calc.py
+++ calc.py
@@ -1,6 +1,6 @@
 def add(a,b):
     return a + b
 def dodawanie():
-    add(int(input("podaj pierwszą liczbę")), int(input("podaj druga liczbę")))
+    print(add(int(input("podaj pierwszą liczbę ")), int(input("podaj druga liczbę "))))
     
 dodawanie()
